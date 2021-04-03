ff78f288be9b963aa27094211dab7311a1a54179
Ashutosh <ashu.sistec@gmail.com>
2021-04-03 15:39:45 +0530

    @Component @Title @Created Title Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Title from './Components/Title/title'
 
 class App extends Component {
   render() {
-    return(
+    return (
       <React.Fragment>
-        <p>App Started</p>
+        <Title />
       </React.Fragment>
     );
   }
---
+++ src/Components/Title/title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './title.css';
+
+const Title = (props) => {
+    return(
+        <div className='title'>
+            <p>set<span className='yellow'>imen</span>t analysis from text</p>
+        </div>
+    );
+}
+
+export default Title;
