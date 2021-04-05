f3049cb658fccc702a8096287e71314c86c3cdec
Ashutosh <ashu.sistec@gmail.com>
2021-04-05 15:02:55 +0530

    @Component @Error, Added Error

---
+++ src/Components/Error/error.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./error.css";
+import Title from "../Title/title";
+import Contact from "../Contact/contact";
+import Button from "../Button/button";
+import Notify from "../Notify/notify";
+
+const Error = () => {
+  return (
+    <React.Fragment>
+      <Title />
+      <Contact />
+      <Notify text="Page Not Found" />
+      <a href="/">
+        <Button text="back" function={() => this.viewResult()} />
+      </a>
+    </React.Fragment>
+  );
+};
+
+export default Error;
--- src/Route/path.js
+++ src/Route/path.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Home from '../Components/Home/home';
 import Result from '../Components/Result/result';
+import Error from '../Components/Error/error';
 import { Switch, Route } from 'react-router-dom';
 
 class Router extends Component {
@@ -10,6 +11,7 @@ class Router extends Component {
         <Switch>
             <Route path='/' component={ Home } exact/>
             <Route path='/result' component={ Result } exact/>
+            <Route component={ Error } />
         </Switch>
       </React.Fragment>
     );
